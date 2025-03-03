0a789c1a5fed5234f43d265734c61d90f0295cf4
xgeesk <xgeesk@gmail.com>
2025-03-04 00:30:37 +0800

    n22

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,14 +1,12 @@
-<script setup>
+<script setup lang="ts">
 import { RouterLink } from 'vue-router'
-defineProps({
-	class: String,
-	links: /** @type {import('vue').PropType<{ path: string, name: string }[]>} */(Array),
-})
+type LinkInfo = { path: string, name: string }
+const props = defineProps<{ class?: string; links: LinkInfo[] }>()
 </script>
 
 <template>
-	<aside class="flex-col gap-4 p-2" :class="$props.class">
-		<RouterLink v-for="r of $props.links" :to="r.path" :key="r.path" class="hover:text-gray-600">
+	<aside class="flex-col gap-4 p-2" :class="props.class">
+		<RouterLink v-for="r of props.links" :to="r.path" :key="r.path" class="hover:text-gray-600">
 			{{ r.name }}
 		</RouterLink>
 		<hr class="border-gray/40" />
